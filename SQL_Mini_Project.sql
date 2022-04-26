d1a358bbf625ccf4c49010b8a153327921d0c7d9
Nahiyan Bin Noor <90473181+Nahiyan140212@users.noreply.github.com>
2022-04-26 17:23:58 -0500

    Update SQL_Mini_Project.sql

--- SQL_Mini_Project.sql
+++ SQL_Mini_Project.sql
@@ -1,5 +1,5 @@
 --Question 1:
---Top 5 Borough based on Offense Description Combinition.
+--Get the top 5 Borough based on Offense Description Combinition.
 
 SELECT BORO_NM, OFNS_DESC, COUNT(*) AS incident_number
 FROM "data_district_crime"
@@ -10,6 +10,19 @@ LIMIT 5
 --Question 2:
 --How many incidents did occurred per hour?
 
-SELECT h, COUNT(*) FROM "data_district_crime" GROUP BY h  ORDER BY CAST(h AS unsigned) ASC
+SELECT h, 
+COUNT(*) 
+FROM "data_district_crime" 
+GROUP BY h  
+ORDER BY CAST(h AS unsigned) ASC
 
-This line of code shows the number of crime for each hour.
+--This line of code shows the number of crime for each hour.
+
+--A crime was happend in late March, 2018 which was a burglary at a chair store in the Bronx. Get the Date, Time, Latitude and Longitude of the crime location.
+SELECT CMPLNT_TO_DT, CMPLNT_TO_TM, Longitude, Latitude  
+FROM 'data_district_crime'  
+WHERE BORO_NM  LIKE 'Bronx' 
+AND OFNS_DESC LIKE 'burglary' 
+AND PREM_TYP_DESC LIKE 'CHAIN STORE' 
+ORDER BY CMPLNT_TO_DT DESC  
+LIMIT 1;
